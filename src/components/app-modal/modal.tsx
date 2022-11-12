9391390d820f19715422c6dfbbe27332461b89b4
PabloGomezBr <gomezbravopablo@gmail.com>
2022-11-12 23:38:41 +0100

    better connection handler

--- src/components/app-modal/modal.tsx
+++ src/components/app-modal/modal.tsx
@@ -1,6 +1,5 @@
 import { IonButton, IonCard, IonCardContent, IonCardHeader } from '@ionic/react';
 
-import { TELEGRAM_ANDROID_LINK } from '../../config/config';
 import './modal.css';
 
 const Modal = ({ handleClose, show }: any) => {
@@ -14,11 +13,17 @@ const Modal = ({ handleClose, show }: any) => {
                 </IonCardHeader>
                 <IonCardContent>
                     <ol className="instructions-list">
-                        <li>Open <a href={TELEGRAM_ANDROID_LINK} target="_blank" rel="noreferrer">Telegram</a></li>
+                        <li>Open Telegram</li>
                         <li>Find @ZenoyBot and start conversation</li>
-                        <li>Type command: <b><i>/message </i></b>&lt;your text&gt;</li>
+                        <li>Type command: <b><i>/mensaje </i></b>&lt;your text&gt;</li>
                         <li>See magic!</li>
                     </ol>
+                    <ul className="instructions-list">
+                        <li>Click on "Try again" button or the message shown to refresh the app manually</li>
+                        <li>Logo in <span style={{ color: 'red' }}>red</span> means the server is down</li>
+                        <li>Logo in <span style={{ color: '#61DBFB' }}>blue</span> means the server is up</li>
+                        <li>Click down on "Tele-G-Bot" for credits</li>
+                    </ul>
                     <IonButton size='small' color={'dark'} onClick={handleClose} fill="solid">Got it</IonButton>
                 </IonCardContent>
             </IonCard>
--- src/pages/Message.tsx
+++ src/pages/Message.tsx
@@ -18,6 +18,14 @@ class Message extends React.Component<{className: string}, { message: string; so
 
     async componentDidMount() {
         await this.fetchData();
+        this.state.socket.on('connect', () => {
+            this.setState({ isServerUp: false });
+            document.getElementsByClassName('App-logo')[0].classList.remove('error-logo');
+        });
+        this.state.socket.on('connect_error', (err) => {
+            this.setState({ isServerUp: false });
+            document.getElementsByClassName('App-logo')[0].classList.add('error-logo');
+        });
         this.state.socket.on('message', (data) => {
             this.setState({ message: data.message });
         });
@@ -27,15 +35,19 @@ class Message extends React.Component<{className: string}, { message: string; so
 
     async fetchData() {
         this.setState({ isLoading: true });
-        try {
-            const response = await fetch(API_ENDPOINT);
-            const json = await response.json();
-            this.setState({ message: json.message });
-            this.setState({ isServerUp: true });
-        } catch (error) {
-            this.setState({ isServerUp: false });
-        }
-        this.setState({ isLoading: false });
+        setTimeout(async () => {
+            try {
+                const response = await fetch(API_ENDPOINT);
+                const json = await response.json();
+                this.setState({ message: json.message });
+                this.setState({ isServerUp: true });
+                document.getElementsByClassName('App-logo')[0].classList.remove('error-logo');
+            } catch (error) {
+                this.setState({ isServerUp: false });
+                document.getElementsByClassName('App-logo')[0].classList.add('error-logo');
+            }
+            this.setState({ isLoading: false });
+        }, 800);
     }
 
     rederContent() {
@@ -44,7 +56,11 @@ class Message extends React.Component<{className: string}, { message: string; so
                 return (
                     <div>
                         {this.state.message !== '' ? (
-                            <p>{this.state.message}</p>
+                            <div>
+                                <p onClick={() => this.fetchData()}>
+                                    {this.state.message}
+                                </p>
+                            </div>
                         ) : (
                             <p>
                                 <IonButton className="try-btn" onClick={() => this.fetchData()}>Try again</IonButton>
@@ -60,7 +76,7 @@ class Message extends React.Component<{className: string}, { message: string; so
             );
         }
         return (
-            <p style={{ marginTop: '29px' }}>
+            <p style={{ marginTop: '9.7px' }}>
                 <IonSpinner name="crescent" />
             </p>
         );

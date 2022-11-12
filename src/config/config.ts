d60258cc5046443414e2a470a6c4e9a481a2704d
PabloGomezBr <gomezbravopablo@gmail.com>
2022-11-12 03:01:14 +0100

    fix app cors

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 export const ENDPOINT = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
     ? 'http://localhost:3001'
-    : 'https://Tele-G-Bot.up.railway.app';
+    : 'https://tele-g-bot.up.railway.app';
 
 export const API_ENDPOINT = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
     ? 'http://localhost:3001/api'
-    : 'https://Tele-G-Bot.up.railway.app/api';
+    : 'https://tele-g-bot.up.railway.app/api';
--- src/pages/Message.tsx
+++ src/pages/Message.tsx
@@ -12,16 +12,15 @@ class Message extends React.Component<unknown, {message: string, socket: Socket}
         };
     }
 
-    componentDidMount() {
+    async componentDidMount() {
+        await this.fetchData();
         this.state.socket.on('message', (data) => {
-            console.log('SOCKET: ', data)
             this.setState({message: data.message});
         });
-        this.fetchData();
         return () => this.state.socket.close();
     }
 
-    fetchData = async () => {
+    async fetchData(){
         try {
             const response = await fetch(API_ENDPOINT);
             const json = await response.json();
@@ -29,14 +28,19 @@ class Message extends React.Component<unknown, {message: string, socket: Socket}
         } catch (error) {
             console.log('error', error);
         }
-    };
+    }
 
     render(){
         return (
             <>
                 <div>
-                    {this.state.message ? <p>{this.state.message}</p> : <IonSpinner name="crescent"/>}
+                    {this.state.message !== '' ? <p>{this.state.message}</p> : <p><IonSpinner name="crescent"/></p>}
                 </div>
+                <a href="https://github.com/PabloGomezBr/tele-g-bot-server" target="_blank" rel="noreferrer" style={{ position: 'absolute', bottom: 0, color: 'yellow'}}>
+                    <i style={{ position: 'relative', bottom: '5px', fontWeight: '900', 'fontSize': '15px'}}>
+                        &#128073; Tele-G-Bot &#128072;
+                    </i>
+                </a>
             </>
         )
     }
